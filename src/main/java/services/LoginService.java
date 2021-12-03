239254531d577b9e27b6730943cb3dd3ba61a452
JoanaBugarin <wooli3000@gmail.com>
2021-12-03 13:47:40 -0300

    terminado de adaptar loginService

--- src/main/java/services/LoginService.java
+++ src/main/java/services/LoginService.java
@@ -1,16 +1,17 @@
 package services;
 
+import java.sql.SQLException;
+
 import model.Usuario;
 import model.nullobjects.NullUser;
-import persistence.UserDAO;
 import persistence.UsuarioDAO;
 import persistence.commons.DAOFactory;
 
 public class LoginService {
 
-	public Usuario login(String nombre, String password) {
+	public Usuario login(String nombre, String password) throws SQLException {
 		UsuarioDAO usuarioDao = DAOFactory.getUserDAO();
-    	Usuario usuario = usuarioDao.findByUsername(nombre);
+    	Usuario usuario = usuarioDao.findByNombre(nombre);
     	
     	if (usuario.isNull() || !usuario.checkPassword(password)) {
     		usuario = NullUser.build();
