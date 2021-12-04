d6d6d1edf94d9e9eb6433943d1c303360fdc8bef
JoanaBugarin <wooli3000@gmail.com>
2021-12-04 15:57:48 -0300

    trabajando en nulluser

--- src/main/java/model/nullobjects/NullUser.java
+++ src/main/java/model/nullobjects/NullUser.java
@@ -1,15 +1,16 @@
 package model.nullobjects;
 
+import model.TipoAtraccion;
 import model.Usuario;
 
 public class NullUser extends Usuario {
 
-	public static Usuario build() {
+	public static Usuario build() throws Exception {
 		return new NullUser();
 	}
 	
-	public NullUser() {
-		super(0, "", "", 0, 0.0, false);
+	public NullUser() throws Exception {
+		super("", 0.0, 0.0, (TipoAtraccion)null, "", false);
 	}
 	
 	public boolean isNull() {
