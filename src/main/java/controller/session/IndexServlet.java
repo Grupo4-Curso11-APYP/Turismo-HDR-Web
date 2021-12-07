782b1383bfa38960b840441ac956dd2e190ff912
JoanaBugarin <wooli3000@gmail.com>
2021-12-07 15:15:41 -0300

    trabajando en index

---
+++ src/main/java/controller/session/IndexServlet.java
@@ -0,0 +1,10 @@
+package controller.session;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/index.do")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = -4819822600796550037L;
+
+}
